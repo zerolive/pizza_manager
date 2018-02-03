fe2c7e31af64327d1d9127a5388710c4595f262f
zerolive <zerolive@gmail.com>
2018-02-03 18:25:32 +0100

    Add pizzas to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+PreparedPizza.create(name: 'The​ ​Super​ ​Mushroom​ ​Pizza')
+PreparedPizza.create(name: 'The​ ​Fun​ Pizza​')
--- spec/features/pizzas_spec.rb
+++ spec/features/pizzas_spec.rb
@@ -3,7 +3,7 @@ require 'capybara'
 
 describe 'Pizzas' do
   it 'are shown' do
-    fun_pizza_name ='The​ ​Fun​ Pizza​'
+    fun_pizza_name = 'The​ ​Fun​ Pizza​'
     mushroom_pizza_name = 'The​ ​Super​ ​Mushroom​ ​Pizza'
     PreparedPizza.create(name: fun_pizza_name)
     PreparedPizza.create(name: mushroom_pizza_name)
