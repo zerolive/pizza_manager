a83877a70951e42c9fca4cfbb54eac552b884e93
zerolive <zerolive@gmail.com>
2018-02-03 21:02:37 +0100

    Pizza has to have an unique name

--- app/models/prepared_pizza.rb
+++ app/models/prepared_pizza.rb
@@ -1,3 +1,3 @@
 class PreparedPizza < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/prepared_pizza_spec.rb
+++ spec/models/prepared_pizza_spec.rb
@@ -12,4 +12,21 @@ describe PreparedPizza do
     expect(errors.count).to eq(1)
     expect(errors.messages[:name]).to eq(["can't be blank"])
   end
+
+  it 'has to have an unique name' do
+    pizza_name = 'Any name'
+    pizza = PreparedPizza.new(
+      name: pizza_name
+    )
+    pizza.save
+    repeated_pizza = PreparedPizza.new(
+      name: pizza_name
+    )
+
+    repeated_pizza.save
+
+    errors = repeated_pizza.errors
+    expect(errors.count).to eq(1)
+    expect(errors.messages[:name]).to eq(["has already been taken"])
+  end
 end
