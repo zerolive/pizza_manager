303fa1f444e2279a37945ae0b611874f9e9b0d7a
zerolive <zerolive@gmail.com>
2018-02-03 20:57:47 +0100

    Ingredient has to have an unique name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
   validates :price, presence: true
 end
--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -27,4 +27,24 @@ describe Ingredient do
     expect(errors.count).to eq(1)
     expect(errors.messages[:price]).to eq(["can't be blank"])
   end
+
+  it 'has to have an unique name' do
+    any_name = 'Any name'
+    any_price = '1'
+    ingredient = Ingredient.new(
+      name: any_name,
+      price: any_price
+    )
+    ingredient.save
+    repeated_ingredient = Ingredient.new(
+      name: any_name,
+      price: any_price
+    )
+
+    repeated_ingredient.save
+
+    errors = repeated_ingredient.errors
+    expect(errors.count).to eq(1)
+    expect(errors.messages[:name]).to eq(["has already been taken"])
+  end
 end
