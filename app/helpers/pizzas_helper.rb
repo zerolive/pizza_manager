452117143d72a32acb004d5c86126e6daf2b7f8d
zerolive <zerolive@gmail.com>
2018-02-04 17:54:48 +0100

    Removed unused code

--- app/helpers/pizzas_helper.rb
+++ app/helpers/pizzas_helper.rb
@@ -1,11 +1,2 @@
 module PizzasHelper
-  def calculate_pizza_price(ingredients)
-    ingredients_price = 0.0
-
-    ingredients.each do |ingredient|
-      ingredients_price += ingredient.price
-    end
-
-    final_price = ingredients_price * 1.5
-  end
 end
