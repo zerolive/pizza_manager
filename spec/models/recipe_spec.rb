b5500f7abaaff8a69b12640bbd06e03270a5fe96
zerolive <zerolive@gmail.com>
2018-02-04 18:39:55 +0100

    Test recipe ingredients for pizza

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -28,4 +28,18 @@ describe Recipe do
     expect(errors.count).to eq(1)
     expect(errors.messages[:ingredient]).to eq(["can't be blank"])
   end
+
+  it 'can return the ingredients for a pizza' do
+    pizza = PreparedPizza.new(name: 'Any pizza')
+    pizza.save
+    ingredient = Ingredient.new(name: 'Any ingredient', price: 1.0)
+    ingredient.save
+    recipe = Recipe.new(pizza: pizza.id, ingredient: ingredient.id)
+    recipe.save
+    ingredients_list = [ingredient.id]
+
+    ingredients = Recipe.ingredients_for(pizza.id)
+
+    expect(ingredients).to eq(ingredients_list)
+  end
 end
